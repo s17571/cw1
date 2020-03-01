09c2ccfa14300067b4e49116c781c742fda68053
Michał Kołnierzak <s17571@pjwstk.edu.pl>
2020-03-01 19:08:56 +0100

    Working programme

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var emails = await GetEmails(args[0]);
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+
+            var response = await httpClient.GetAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            //find items that matches with our pattern
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+            foreach (var emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+
+            return listOfEmails;
+        }
+    }
+}
